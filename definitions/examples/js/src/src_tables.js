dcdb292de3beefcecebc6778fe21b5519fc768df
Moukhtar Ahmed <moukhtar@google.com>
2023-03-15 00:22:38 +0100

    adding sql example

--- definitions/examples/js/src/src_tables.js
+++
@@ -1,9 +0,0 @@
-schema_name = "citibike_source"
-tables_prefix = "src_"
-sources_tables = models.get_all()
-
-dvform.create_source_tables(
-    schema_name, 
-    tables_prefix, 
-    sources_tables
-)
